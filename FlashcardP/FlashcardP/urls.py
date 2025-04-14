070a07ae9ea642e1d880459968da7c6281bae827
Daniii ♡ <danielisvrojassanchez06@gmail.com>
2025-04-14 13:25:34 -0400

    AC 1.2

--- FlashcardP/FlashcardP/urls.py
+++ FlashcardP/FlashcardP/urls.py
@@ -6,7 +6,7 @@ from App.views import SignUpView, LoginView, flashcards_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('flashcards/', include('App.urls')),
-    path('flashcards/', flashcards_view, name='flashcards'),
+    path('flashcards/', flashcards_view),
     path('signup/', SignUpView.as_view()),
     path('login/', LoginView.as_view()),
 ]
